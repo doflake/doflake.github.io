bc134eef17969822e5173fd59016dfe969bf83e2
doflake <1242803739@qq.com>
2021-05-21 12:41:02 +0800

    Site updated: 2021-05-21 12:41:01

--- js/totop.js
+++ js/totop.js
@@ -5,16 +5,7 @@ function backToTop(){
 }
 window.onscroll = function(){
     var scrollTop = this.document.documentElement.scrollTop || this.document.body.scrollTop;
-	var totopBtn = this.document.getElementById("totop");
-    if(scrollTop < 200){
-        totopBtn.style.opacity=scrollTop/200;
-        if(totopBtn.style.opacity<0.1) totopBtn.style.display="none";
-        else totopBtn.style.display="inline";
-    }
-	else
-	{
-		totopBtn.style.opacity=scrollTop/200;
-		totopBtn.style.display="inline";
-        totopBtn.addEventListener('click',this.backToTop,false);
-	}
+    var totopBtn = this.document.getElementById("totop");   
+    totopBtn.style.display="inline";
+    totopBtn.addEventListener('click',this.backToTop,false);
 }
